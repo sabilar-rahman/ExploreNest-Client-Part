421558146faa6567d5ea6572f2dc2c1f00186374
sabilar-rahman <sabilarrahman36@gmail.com>
2024-11-02 10:13:39 +0600

    user and admin dashboard sidebar

--- src/components/modules/dashboard/Sidebar.tsx
+++ src/components/modules/dashboard/Sidebar.tsx
@@ -1,5 +1,39 @@
-import React from 'react';
+"use client";
 
+import React from "react";
+
+const UserSidebarLinks = [
+  {
+    item: "User Profile",
+    link: "/dashboard/profile",
+  },
+  { item: "My Posts", link: "/dashboard/posts" },
+  {
+    item: "Edit My Profile",
+    link: "/dashboard/edit-profile",
+  },
+];
+
+const AdminSidebarLinks = [
+  {
+    item: "Admin Profile",
+    link: "/dashboard/profile",
+  },
+  { item: "Payment History", link: "admin/dashboard/payment" },
+  {
+    item: "Posts Management",
+    link: "/admin/dashboard/post-management",
+  },
+  { item: "My Content", link: "/dashboard/my-content" },
+  { item: "Bookmark", link: "/dashboard/bookmark" },
+  { item: "Edit My Profile", link: "/dashboard/edit-profile" },
+  //   {
+  //     item: "Change Password",
+  //     link: "/dashboard/change-password",
+  //   },
+
+  { item: "Home", link: "/" },
+];
 const Sidebar = () => {
   return (
     <div>
